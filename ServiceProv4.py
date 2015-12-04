32e55c1d70c258a3448664813df13655b4f30be7
Steven Lehar <slehar@gmail.com>
2015-12-04 13:34:03 -0500

    Fix plot trace SP4 and add text and Elight

--- ServiceProv4.py
+++ ServiceProv4.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.widgets import CheckButtons, Slider
 from collections import deque
-import time
+# import time
 
 # Global Variables
 flow = False
@@ -22,8 +22,9 @@ dt = .5
 A = .01
 delay = 0.1     # sec per cycle
 refill = 0.001
-darray = deque(np.arange(0., 9., .1))
-tarray = deque(np.arange(0., 9., .1))
+dArray = deque([0.])
+tArray = deque([0.])
+plotWidth = 500
 flevel = 1.0
 lastflevel = 1.0
 empty = False
@@ -49,6 +50,9 @@ circle = plt.Circle((6, 2), 1, fc='r', ec='k')
 ax.add_patch(circle)
 arrow = plt.Arrow(3, 2, 2, 0, ec='k', fc=(1, 1, 1))
 ax.add_patch(arrow)
+Elight = ax.text(.7, 1, r"E", fontsize=18, color='r', visible=False)
+
+ax.text(1.8,3.5, 'Supply deplete and refill', fontsize=14, weight='bold')
 
 # Add Input checkbox as axes Ch
 axCh = fig.add_axes([.6, .4, .1, .1])
@@ -77,12 +81,12 @@ def func(label):
 check.on_clicked(func)
 
 # Add axes 2 for plot trace
-ax2 = fig.add_axes([.1, .1, .8, .2])
-ax2.set_ylim(0, 1)
-ax2.set_xlim(0, 10)
+axTime = fig.add_axes([.1, .1, .8, .2])
+axTime.set_ylim(0, 1)
+axTime.set_xlim(0, 10)
 
 # Set up plot line in axes 2
-line, = ax2.plot(t, xx, color='blue', linewidth=1,
+line, = axTime.plot(t, xx, color='blue', linewidth=1,
                  linestyle='-', alpha=1.0)
 
 # Add Input slider
@@ -91,19 +95,21 @@ axSl.set_xticklabels([])
 axSl.set_yticklabels([])
 axSl.set_xticks([])
 axSl.set_yticks([])
-sl = Slider(axSl, 'Mag', 0., 1., valinit=0.5, valfmt=u'%1.2f', fc=(0, 1, 0))
+sl = Slider(axSl, 'Mag', 0., 5., valinit=1., valfmt=u'%1.2f', fc=(0, 1, 0))
 
 
 # Update each loop
 def update(num):
     global xx, flevel, empty, t, lastX, lastT, flow, valveOpen, refill, delay
     global darray, tarray
+    global Elight
     
     if valveOpen and not empty:
         I = 0.1 * sl.val
         flevel -= 0.1
         if flevel < 0.1:
             flevel = 0.
+            Elight.set_visible(True)
             empty = True
             flow = False
     else:
@@ -129,15 +135,15 @@ def update(num):
     circle.set_facecolor((r, g, 0.))
     lastT = t
     t += dt
-    darray.appendleft(xx)
-    darray.pop()
-    tarray.appendleft(t)
-    tarray.pop()
-    ax2.set_xlim(tarray[0], tarray[len(tarray)-1])
-    # line.set_data([.1,0], [lastX,x])
-    line.set_data(tarray, darray)
-    # CurrentXAxis=np.arange(len(values)-100,len(values),1)
-    ax2.axis()
+    dArray.appendleft(xx)
+    if len(dArray) >= plotWidth/dt:
+        dArray.pop()
+    tArray.appendleft(t)
+    if len(tArray) >= plotWidth/dt:
+        tArray.pop()
+    line.set_data(tArray,dArray)
+    axTime.axis((t - plotWidth, t, 0., 1.))
+    # time.sleep(.1)
     
     valveOpen = False  # Shut valve after treatment
     check.lines[0][0].set_visible(False)
@@ -145,10 +151,11 @@ def update(num):
     flevel += refill
     if flevel > .1:
         empty = False
+        Elight.set_visible(False)
     if flevel > 1.:
         flevel = 1
 
-    time.sleep(delay)
+    # time.sleep(delay)
 
 
 # Run the animation
