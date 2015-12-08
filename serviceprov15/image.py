1e7868be233f3d5bea0b56f1c8cbf7fda2c4f712
Steven Lehar <slehar@gmail.com>
2015-12-08 17:28:41 -0500

    SP15 fix background map

--- serviceprov15/image.py
+++ serviceprov15/image.py
@@ -27,13 +27,12 @@ def init_map():
     global aspect, mapImg, maskImg, burrImg, burrIndx, imgXSize, imgYSize, imgZSize, xOff
     
     # Display Background Map
-    # burrImg = plt.imread('BurroughsIndex.png')
-    # (imgYSize, imgXSize, imgZSize) = burrImg.shape
-    burrImg = Image.open('BurroughsIndx.png')
+    burrImg = Image.open('BurroughsIndxNoBg.png')
     (imgYSize, imgXSize) = (burrImg.height, burrImg.width)
         
     burrIndx = np.array(burrImg.convert('P'))
     
+    # Clean up partial-surface pixels
     for y in range(imgYSize):
         for x in range(imgXSize):
             if burrIndx[y,x] not in (0, 1, 2, 3, 4, 5):
@@ -43,10 +42,18 @@ def init_map():
     aspect = float(imgXSize)/float(imgYSize)
     (xScale, yScale) = (imgXSize * aspect / imgXSize, 1.)
     xOff = (1. - xScale)/2.
+    
+    # Load Background Image
+    backImg = plt.imread('UHF42EdMap.png')
+    axes.ax.imshow(backImg, extent=[xOff, xOff+xScale, 0, yScale], alpha=1)
+    
+    # Display Boroughs Index Map
     axes.ax.imshow(burrImg, extent=[xOff, xOff+xScale, 0, yScale])
     
     # Load mask map
     maskImg = plt.imread('BurroughsMask.png')
-    axes.ax.imshow(maskImg, extent=[xOff, xOff+xScale, 0, yScale], alpha=.5)
+    # axes.ax.imshow(maskImg, extent=[xOff, xOff+xScale, 0, yScale], alpha=.5)
+    
+    
 
 
